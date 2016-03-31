52169007fa4d312a5c831f4479f8da905f21d6f0
xuyuming <clark_xym@163.com>
2016-03-31 08:28:25 +0800

    新增证券保证金余额变动抓取信息

---
+++ pythonbasic/htmlcrawler.py
@@ -0,0 +1,45 @@
+#coding=utf-8
+__author__ = 'xuyuming'
+from urllib import urlopen
+from BeautifulSoup import BeautifulSOAP
+from bs4 import BeautifulSoup
+text=urlopen('http://www.sipf.com.cn/subject/sub_ch/tjsj/index.html').read()
+soup=BeautifulSoup(text)
+print soup.originalEncoding
+soup.prettify()
+for tabb in  soup.findAll('table')[1:2]:
+     tr_garanteebal=tabb.findAll('tr')[2]
+     date=tr_garanteebal.findAll('p')[0].string #获取证券交易结算资金的日期时间段
+     endvolume= tr_garanteebal.findAll('p')[2].string#获取证券交易结算资金的期末余额
+     avgvolume=tr_garanteebal.findAll('p')[3].string#获取证券交易结算资金的平均余额
+     involume=tr_garanteebal.findAll('p')[4].string#获取证券交易结算资金的转入额
+     outvolume=tr_garanteebal.findAll('p')[5].string#获取证券交易结算资金的转出额
+     print date[0:4]+date[11:13]+date[-2:]+' '+endvolume+' '+avgvolume+' '+involume+' '+outvolume
+#接下来将
+
+# 四大对象种类
+# Tag
+# NavigableString
+# BeautifulSoup
+# Comment
+
+
+#以下是证券交易结算资金的变动情
+ # <TR>
+ #            <TD rowSpan=3 width=100 colSpan=2>
+ #                <P align=center><font size=3>2016.03.21-03.25</font></P></TD>
+ #            <TD vAlign=center width=130>
+ #                <P align=center><font size=3>证券交易结算资金</font></P></TD>
+ #            <TD vAlign=center width=90>
+ #                <P align=center><font size=3>17,287</font></P></TD>
+ #            <TD vAlign=center width=90>
+ #                <P align=center><font size=3>17,718</font></P></TD>
+ #            <TD vAlign=center width=100>
+ #                <P align=center><font size=3>5,667</font></P></TD>
+ #            <TD vAlign=center width=100>
+ #                <P align=center><font size=3>5,608</font></P></TD>
+ #            <TD vAlign=center width=120>
+ #                <P align=center><font size=3>59</font></P></TD>
+ #            <TD vAlign=center width=120>
+ #                <P align=left><font size=3></font></P></TD>
+ #        </TR>
