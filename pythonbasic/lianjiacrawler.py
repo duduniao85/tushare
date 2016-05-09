a8ccdbe5ebc14a2734c911ed408055df45d22a63
Xu Yuming <283548048@qq.com>
2016-05-09 23:46:03 +0800

    测试VCS常用操作

--- pythonbasic/lianjiacrawler.py
+++ pythonbasic/lianjiacrawler.py
@@ -7,10 +7,18 @@ textdata=urlopen('http://sh.lianjia.com/chengjiao/d1000').read()#取官方网站
 soup=BeautifulSoup(textdata,'lxml')
 # print soup
 # body > div.wrapper > div.main-box.clear > div > div.list-wrap > ul > li:nth-child(1) > div.info-panel > h2 > a
+# 摸拟登录
+# 首先启动一个循环，确保每一笔成交都能抓到
 basicinfos=soup.select('body > div.wrapper > div.main-box.clear > div > div.list-wrap > ul > li > div.info-panel > h2 > a')
-quxians=soup.select('body > div.wrapper > div.main-box.clear > div > div.list-wrap > ul > li > div.info-panel > div > div.col-1.fl > div.other > div > a')
+quxians=soup.select('body > div.wrapper > div.main-box.clear > div > div.list-wrap > ul > li > div.info-panel > div > div.col-1.fl > div.other > div ')
+qianyueriqis=soup.select('body > div.wrapper > div.main-box.clear > div > div.list-wrap > ul > li > div.info-panel > div > div.col-2.fr > div > div:nth-of-type(1) > div')
+qianyuedanjias=soup.select()
+qianyuezongjia=soup.select()
 for quxian in quxians:
-    print list(quxian.stripped_strings)
+    print list((quxian.stripped_strings))[0],list((quxian.stripped_strings))[1],list((quxian.stripped_strings))[3].split(r'/')[0],list((quxian.stripped_strings))[3].split(r'/')[1],\
+    list((quxian.stripped_strings))[5]
+for qianyueriqi in qianyueriqis:
+    print qianyueriqi.get_text()
 
 # 筛选所需信息
 for basicinfo in basicinfos:
